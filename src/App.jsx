c58cdce8ed25da478ee6276ae602b46ceb525a98
Ömer temel <42616536+Puyz@users.noreply.github.com>
2024-08-07 00:09:19 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as signalR from '@microsoft/signalr';
 import 'react-toastify/dist/ReactToastify.css';
+import { Button, Input, Flex, Space } from 'antd';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
 
 function App() {
- 
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
- return (
-  <div>
-    RabbitMQ
-  </div>
- );
+  const gonder = () => {
+
+    const userData = {
+      Email: email,
+      Message: message
+    };
+    axios.post("https://localhost:7047/api/messages", userData);
+  };
+  useEffect(() => {
+    const connection = new signalR.HubConnectionBuilder()
+    .withUrl("https://localhost:7047/messagehub")
+    .withAutomaticReconnect()
+    .build();
+
+    connection.start();
+    connection.on("receiveMessage", message => {
+      toast.success(message);
+    });
+  }, []);
+
+  return (
+    <div>
+      <ToastContainer />
+      <Space>
+        <Input placeholder='Email' style={{ width: 300 }} onChange={(e) => setEmail(e.target.value)} value={email} />
+        <Input placeholder='Mesaj' style={{ width: 300 }} onChange={(e) => setMessage(e.target.value)} value={message} />
+        <Button onClick={gonder}>Gönder</Button>
+      </Space>
+    </div>
+  );
 }
 
 export default App;
