4ac0476d2a95630d6628197b9e8ae1480d3679cc
Ömer temel <42616536+Puyz@users.noreply.github.com>
2024-08-08 23:20:27 +0300

    update

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  name: string = "";
+  constructor(private http: HttpClient) { }
+
+  login() {
+
+  }
+}
